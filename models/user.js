7b94c6d5b3255e59914e69ea3eecf57952e47756
Micah Bottoms <bottoms.micah.3@gmail.com>
2022-10-06 18:44:15 -0600

    debugged errors

--- models/user.js
+++ models/user.js
@@ -1,57 +1,53 @@
 const { SchemaTypes } = require("mongoose");
 
-const { Schema, model } = reqire('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trimmed: true,
-        },
-
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Invalid email']
-        },
-
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought'
-            },
-        ],
-
-        
-
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            },
-        ],
-        
-
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
     },
 
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-    }
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Invalid email",
+      ],
+    },
 
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
 
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
 
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
-userSchema.virtual('friendCount').get(function() {
-    return this.friends.length;
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-const User = model('user', userSchema);
+const User = model("user", userSchema);
 
 module.exports = User;
